fb8e7346f768ffb872bdb6230682e51b89ad6d19
Kyle Lanchman <k.lanchman@gmail.com>
2024-03-25 16:52:33 -0400

    Add swift-format and SwiftLint

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,7 +5,7 @@ func routes(_ app: Application) throws {
         try await req.view.render("index", ["title": "Hello Vapor!"])
     }
 
-    app.get("hello") { req async -> String in
+    app.get("hello") { _ async -> String in
         "Hello, world!"
     }
 }
